864aa1cfaa091d858331af3f4e04886b8d56f46d
Altison <71751499+Altison@users.noreply.github.com>
2020-10-21 15:25:11 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= 4; j++) {
+            if (I % 2 == j % 2) {
+                led.plot(I, j)
+            }
+        }
+    }
+})
