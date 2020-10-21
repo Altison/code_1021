53a7d6e86e351ddb113f4e08b1133304f7ba5024
Altison <71751499+Altison@users.noreply.github.com>
2020-10-21 15:43:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let value = false
 input.onButtonPressed(Button.A, function () {
     for (let I = 0; I <= 4; I++) {
         for (let j = 0; j <= 4; j++) {
@@ -5,5 +6,28 @@ input.onButtonPressed(Button.A, function () {
                 led.plot(I, j)
             }
         }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    value = true
+    for (let index = 0; index < 2; index++) {
+        for (let x = 0; x <= 8; x++) {
+            for (let I = 0; I <= 4; I++) {
+                for (let j = 0; j <= 4; j++) {
+                    if (value) {
+                        if (I + j == 8 - x) {
+                            led.plot(I, j)
+                        }
+                    } else {
+                        if (I + j == x) {
+                            led.unplot(I, j)
+                        }
+                    }
+                }
+            }
+            basic.pause(100)
+        }
+        basic.pause(1000)
+        value = false
     }
 })
