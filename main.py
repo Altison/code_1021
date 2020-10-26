fd4058f0ba817bc3e27eec67a820fe825cfc3907
Altison <71751499+Altison@users.noreply.github.com>
2020-10-26 20:59:51 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 value = False
+I = 0
+j = 0
 
 def on_button_pressed_a():
-    for I in range(5):
-        for j in range(5):
-            if I % 2 == j % 2:
-                led.plot(I, j)
+    global value, I, j
+    value = True
+    I = 4
+    j = 4
+    for index in range(2):
+        for x in range(10):
+            y = 0
+            while y <= min(5 - x, x):
+                if value:
+                    led.plot(0, min(4 - x, x))
+                    I += 1
+                    j += 1
+                else:
+                    led.unplot(0, 0)
+                y += 1
+            basic.pause(100)
+            I = 0
+        value = False
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def draw(bool: bool):
+def on_button_pressed_ab():
     for I2 in range(5):
         for k in range(5):
-            x = 0
-            if I2 + k == x:
-                if value:
-                    led.plot(4 - I2, 4 - k)
-                else:
-                    led.unplot(I2, k)
+            if I2 % 2 == k % 2:
+                led.plot(I2, k)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global value
     value = True
-    for index in range(2):
+    for index2 in range(2):
         for x2 in range(9):
-            draw(value)
+            for I3 in range(5):
+                for l in range(5):
+                    if I3 + l == x2:
+                        if value:
+                            led.plot(4 - I3, 4 - l)
+                        else:
+                            led.unplot(I3, l)
+            basic.pause(100)
         value = False
         basic.pause(1000)
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
 let value = false
+let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
+    value = true
+    for (let index = 0; index < 2; index++) {
+        for (let I = 0; I <= 8; I++) {
+            x = Math.max(4 - I, 0)
+            y = Math.min(8 - I, 4)
+            for (let j = 0; j <= Math.min(8 - I, I); j++) {
+                if (value) {
+                    led.plot(x, y)
+                } else {
+                    led.unplot(4 - x, 4 - y)
+                }
+                x += 1
+                y += -1
+            }
+            basic.pause(100)
+        }
+        value = false
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
     for (let I = 0; I <= 4; I++) {
         for (let j = 0; j <= 4; j++) {
             if (I % 2 == j % 2) {
